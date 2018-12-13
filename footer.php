296fd9287bfb2cad8974bedfb9786b5036787810
bayapucg <bayapu.edu004@gmail.com>
2018-12-13 10:33:15 +0530

    adding
    toattal project

---
+++ footer.php
@@ -0,0 +1,173 @@
+
+  <!-- Footer -->
+  <footer>
+    <div class="callout">
+      <div class="container">
+        <div class="row wow fadeInDown" data-wow-delay=".6s">
+          <div class=" col-sm-7">
+            <h2>Want Any Assistence Please Write Us</h2>
+          </div>
+          <div class=" col-sm-5">
+            <a href="#" class="button btn-theme medium ">GET A QUOTE &nbsp;
+              <i class="fas fa-location-arrow"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3 wow fadeInLeft" data-wow-delay=".3s">
+            <div class="themeColor">Stay in touch</div>
+            <div class="formOuter">
+              <form id="footerContactForm" action="#">
+                <input type="text" class="inputText" placeholder="Name" name="namef">
+                <input type="email" class="inputText" placeholder="Email" name="emailf">
+                <textarea class="inputText" placeholder="Write message" name="messagef"></textarea>
+                <input type="submit" value="Submit" class="button btn-theme small button-fill-width">
+              </form>
+              <div class=" error-handling-messages">
+                <div id="success-footer">Thank you</div>
+                <div id="error-footer"> Error occurred while sending email. Please try again later. </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3 wow fadeInLeft" data-wow-delay=".6s">
+            <div class="themeColor">Service we offer</div>
+            <ul>
+              <li>
+                <a href="#">Web Development </a>
+              </li>
+              <li>
+                <a href="#">Mobile / App Development</a>
+              </li>
+              <li>
+                <a href="#">Marketing</a>
+              </li>
+              <li>
+                <a href="#">Solutions</a>
+              </li>
+              <li>
+                <a href="#">Market Verticals</a>
+              </li>
+            </ul>
+            <div class="themeColor">Work For</div>
+            <ul>
+              <li>
+                <a href="#">Agency </a>
+              </li>
+              <li>
+                <a href="#">Enterprise </a>
+              </li>
+              <li>
+                <a href="#">Startup</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-sm-3  wow fadeInRight" data-wow-delay="1.2s">
+            <div class="themeColor">Quick Links</div>
+            <ul>
+              <li>
+                <a href="index.php">Home </a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+            <div class="themeColor">Connect with Us</div>
+            <ul class="socialIcon">
+              <li>
+                <a href="#" class="fb">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="tw">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="li">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="gp">
+                  <i class="fab fa-google-plus-g"></i>
+                </a>
+              </li>
+
+            </ul>
+          </div>
+          <div class="col-sm-3 haevQuestion  wow fadeInRight" data-wow-delay="1.6s">
+            <div class="themeColor">Have Question?</div>
+            <div class="font12">Call Us </div>
+            <div class="callNo">IND: 040-48541273
+              </div>
+            <div class="emailIds">
+              <i class="fas fa-envelope"></i> &nbsp;
+              <a href="mailto:info@example.com"> info@prachatech.com</a>
+              <br>
+              <i class="fab fa-skype"></i> &nbsp;
+              <a href="#"> pracha.skype </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Copy Right Section -->
+    <div class="copyRight">
+      <div class="container">
+        <div class="copy">&copy; 2018 Pracha. All Rights Reserved.</div>
+        <div class="designedBy">Website Design & Developed by
+          <a href="#
+  ">Pracha</a>
+        </div>
+      </div>
+    </div>
+    <!-- .Copy Right Section -->
+
+  </footer>
+  <!-- .Footer -->
+
+
+  <!-- Back to top -->
+  <a href="javascript:;" id="back-to-top" title="Back to top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+  <!-- .Back to top -->
+
+
+  <!--jQuery minified v1.11.3 library  -->
+  <script src="js/jquery.min.js"></script>
+
+  <!--jQuery validate JS   -->
+  <script src="js/validate.js"></script>
+
+  <!--Bootstrap minified JS -->
+  <script src="js/bootstrap.min.js"></script>
+
+  <!--Wow Effect JS -->
+  <script src="js/wow.min.js"></script>
+
+  <!--Owl Carousel 2 minified JS -->
+  <script src="js/owl.carousel.min.js"></script>
+
+  <!--Theme Custom JS -->
+  <script src="js/function.js"></script>
+  <script src="js/demo.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="charset=UTF-8" />
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Prachatech</title>
+
+
+  <!-- Favicon  -->
+  <link rel="icon" href="http://prachatech.com/assets/vendor/img/fav.png">
+<link rel="stylesheet" href="css/fontawesome/css/all.css" >
+
+
+  <!-- Bootstrap 3.3.7 CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!--Owl Carousel 2  CSS -->
+  <link href="css/owl.carousel.css" rel="stylesheet">
+
+  <!--Animate CSS for WOW Effects-->
+  <link href="css/animate.css" rel="stylesheet">
+
+  <!--Custom Stylesheet -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/custom.css" rel="stylesheet">
+
+
+  
+  <!--Theme Switcher -->
+  <link href="css/demo.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet" id="theme-switcher">
+
+  <!--Custom Stylesheet -->
+  <link href="css/responsive.css" rel="stylesheet">
+
+
+
+  <!--Google Fonts API-->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,900" rel="stylesheet">
+
+  <!--Font Awesome 5.0.8 for Icons  -->
+  <link href="../../../../use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Page Preloader -->
+  <div class="pagePreloader"> </div>
+  <!-- .Page Preloader -->
+
+
+  <!-- HeaderOuter -->
+  <header class="transparentHeader">
+    <!-- Add .transparentHeader class  for transparent header  -->
+    <!--  Nav Bar  -->
+    <nav class="clearfix navigation">
+      <div class="container clearfix">
+        <div class="row">
+          <div class="col-sm-12">
+            <a href="index.php" class="logo">
+   <img src="images/logo.png" class="" alt="">
+            </a>
+            <div class="navigationTop">
+              <a href="javascript:void(0)" class="mobMenu toogle">
+                <i></i>
+                <i></i>
+                <i></i>
+              </a>
+              <div class="overLayMunu">
+                <img src="images/close-128.png" class="closeMenu" alt=""> </div>
+              <ul class="topMenu topNAv">
+                <li class="activeMenu">
+                  <a href="index.php">Home </a>
+                </li>
+				<li class="activeMenu">
+                  <a href="#">About Us </a>
+                </li>
+                <!--<li class="dropDown megaDropDown">
+                  <a href="#"> Pages (Mega Menu) </a>
+                  <ul class="clearfix">
+                    <div class="subMenuBox col-sm-3">
+                      <div class="blackHD">Blog</div>
+                      <ul class="linkBox">
+                        <li>
+                          <a href="blog-detail.html">Single Blog Page</a>
+                        </li>
+                        <li>
+                          <a href="blog.html">Blog Lising Page</a>
+                        </li>
+                        <li>
+                          <a href="blog-grid.html">Blog Grid Lising</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="subMenuBox col-sm-3">
+                      <div class="blackHD">Portfolio</div>
+                      <ul class="linkBox">
+                        <li>
+                          <a href="portfolio.html">Portfolio</a>
+                        </li>
+                        <li>
+                          <a href="portfolio-2.html">Portfolio 2 Column</a>
+                        </li>
+                        <li>
+                          <a href="portfolio-4.html">Portfolio 4 Column</a>
+                        </li>
+                        <li>
+                          <a href="portfolio-detail.html">Portfolio Detail 1</a>
+                        </li>
+                        <li>
+                          <a href="portfolio-detail2.html">Portfolio Detail 2</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="subMenuBox col-sm-3">
+                      <div class="blackHD">Service Detail Pages</div>
+                      <ul class="linkBox">
+                        <li>
+                          <a href="service1.html">Service Detail Page 1</a>
+                        </li>
+                        <li>
+                          <a href="services2.html">Service Detail Page 2</a>
+                        </li>
+                        <li>
+                          <a href="services3.html">Service Detail Page 3</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="subMenuBox col-sm-3">
+                      <div class="blackHD">Submenu with Icons</div>
+                      <ul class="linkBox">
+                        <li>
+                          <a href="#"> Downloads
+                            <i class="fas fa-download"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#"> Contact Us
+                            <i class="fas fa-envelope"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#"> FAQ
+                            <i class="fas fa-question-circle"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#"> About Us
+                            <i class="fas fa-info"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </ul>
+                </li>-->
+                <li class="dropDown">
+                  <a href="#"> Our Services </a>
+                  <ul>
+                    <li>
+                      <a href="#">Web Designing</a>
+                    </li>
+                    <li>
+                      <a href="#">Web Development</a>
+                    </li>
+                    <li>
+                      <a href="#">Mobile Applications</a>
+                    </li>
+                    <li>
+                      <a href="#">Digital Marketing</a>
+                    </li>
+                    <li>
+                      <a href="#">Software Maintenance</a>
+                    </li>
+                    <li>
+                      <a href="#">E-Commerce</a>
+                    </li>
+                  
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Portfolio </a>
+                </li>
+                <li>
+                  <a href="#">Careers </a>
+                </li> 
+				<li>
+                  <a href="#">Blog </a>
+                </li>
+                <li>
+                  <a href="#">Contact us </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!--  .Nav Bar -->
+  </header>
+  <!-- .HeaderOuter -->
